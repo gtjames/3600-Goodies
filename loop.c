aa02663b0fd3166a400958ef0be32ef1c8d07338
gtjames <gtjames@gmail.com>
2024-08-22 23:15:42 -0500

    oops  left a typo in the code

--- loop.c
+++ loop.c
@@ -25,7 +25,7 @@ int main(int argc, char *argv[]) {
 }
 
 unsigned int getNumber(char *prompt, unsigned int max) {
-    unsigned int    num;`   
+    unsigned int    num;
     char            input[30];
 
     while (1) {
