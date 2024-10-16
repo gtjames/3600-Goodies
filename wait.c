b51aa3454dc7b144def2345629d7ae9f31a3f90d
gtjames <gtjames@gmail.com>
2024-10-15 20:11:25 -0500

    added a few more bells

--- wait.c
+++ wait.c
@@ -5,17 +5,18 @@
 #include <sys/wait.h>
 
 int main() {
-    char *cmd[] = {"who", "ls", "date"}; 
+    char *cmd[] = {"who", "ls", "date"};        //  add anything you like
     char            input[30];
     int i;
     while( 1 ) {
         printf("0=who 1=ls 2=date : ");
+        fflush(stdout);
         fgets(input, sizeof(input), stdin);
         i = atoi(input);
         if(fork() == 0) {
             /* child */
             execlp( cmd[i], cmd[i], (char *)0 ); 
-            printf("execlp failed\n");      //  we only get here on a failure
+            printf("execlp failed: %s\n", cmd[i]);      //  we only get here on a failure
             exit(1);
         }
         else {
